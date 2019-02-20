b6fcc60ac0d2b78794c5a0769087b4a156e72838
Corentin Derbré <loeiz.donal@gmail.com>
2019-02-20 17:36:50 +0800

    sideNav and routes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <v-app>
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
+        <v-router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">Vue Share</h1>
+        </v-router-link>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-tile v-for="item in sideNavItems" :key="item.title" :to="item.link" ripple>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar fixed color="primary" dark>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer" class="hidden-xs-only">Vue Share</router-link>
       </v-toolbar-title>
@@ -24,7 +41,9 @@
     </v-toolbar>
     <main>
       <v-container class="mt-4">
-        <router-view/>
+        <transition name="fade">
+          <router-view/>
+        </transition>
       </v-container>
     </main>
   </v-app>
@@ -33,8 +52,20 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horizontalNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    },
+    sideNavItems() {
       return [
         { icon: "chat", title: "Posts", link: "/posts" },
         { icon: "lock_open", title: "Sign In", link: "/signin" },
@@ -44,3 +75,21 @@ export default {
   }
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: all;
+  transition-duration: 0.2s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.2s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+  transform: translateX(-35px);
+}
+</style>
