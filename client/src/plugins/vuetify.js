4aac7ef548ac3762915076802bc48c325526eb76
Corentin Derbré <loeiz.donal@gmail.com>
2019-02-20 15:14:14 +0800

    vuetify theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -3,5 +3,14 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify, {
+  theme: {
+    primary: "#ff5722",
+    secondary: "#ff9800",
+    accent: "#ffc107",
+    error: "#e91e63",
+    warning: "#673ab7",
+    info: "#607d8b",
+    success: "#8bc34a"
+  },
   iconfont: 'md',
 })
