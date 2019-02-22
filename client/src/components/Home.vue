2536ff37e942eba8474cacb71eff08c55b72b807
Corentin Derbré <loeiz.donal@gmail.com>
2019-02-22 11:04:27 +0800

    carousel

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,16 +1,12 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-
-    <ApolloQuery :query="getPostsQuery">
-      <template slot-scope="{ result: { loading, error, data } }">
-        <div v-if="loading">Loading...</div>
-        <div v-else-if="error">Error! {{ error.message }}</div>
-        <ul v-else v-for="post in data.getPosts" :key="post.id">
-          <li>{{ post.title }}</li>
-        </ul>
-      </template>
-    </ApolloQuery>
+  <v-container text-xs-center v-if="getPosts">
+    <!--<v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carouse__title">{{ post.title }}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>-->
   </v-container>
 </template>
 
@@ -23,23 +19,23 @@ export default {
   name: "home",
   data() {
     return {
-      getPostsQuery: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            categories
-            description
-            createdDate
-            likes
-            createdBy {
-              username
-              avatar
-            }
-          }
-        }
-      `
+      // getPostsQuery: gql`
+      //   query {
+      //     getPosts {
+      //       _id
+      //       title
+      //       imageUrl
+      //       categories
+      //       description
+      //       createdDate
+      //       likes
+      //       createdBy {
+      //         username
+      //         avatar
+      //       }
+      //     }
+      //   }
+      // `
     };
   }
   // apollo: {
@@ -74,3 +70,17 @@ export default {
   // }
 };
 </script>
+
+<style>
+#carouse__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
