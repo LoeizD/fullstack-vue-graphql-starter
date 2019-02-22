7883388b60462e7177d1127e65d595da62127fac
Corentin Derbré <loeiz.donal@gmail.com>
2019-02-22 10:54:36 +0800

    ApolloQuery component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <ul v-for="post in getPosts" :key="post._id">
-      <li>{{ post.title }}</li>
-    </ul>
+
+    <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error">Error! {{ error.message }}</div>
+        <ul v-else v-for="post in data.getPosts" :key="post.id">
+          <li>{{ post.title }}</li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import { gql } from "apollo-boost";
+import { error } from "util";
 
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
+  data() {
+    return {
+      getPostsQuery: gql`
         query {
           getPosts {
             _id
@@ -32,7 +40,37 @@ export default {
           }
         }
       `
-    }
+    };
   }
+  // apollo: {
+  //   getPosts: {
+  //     query: gql`
+  //       query {
+  //         getPosts {
+  //           _id
+  //           title
+  //           imageUrl
+  //           categories
+  //           description
+  //           createdDate
+  //           likes
+  //           createdBy {
+  //             username
+  //             avatar
+  //           }
+  //         }
+  //       }
+  //     `,
+  //     result({ data, loading, networkStatus }) {
+  //       if (!loading) {
+  //         this.posts = data.getPosts;
+  //         console.log("[networkStatus]", networkStatus);
+  //       }
+  //     },
+  //     error(err) {
+  //       console.error("[ERROR]", err);
+  //     }
+  //   }
+  // }
 };
 </script>
